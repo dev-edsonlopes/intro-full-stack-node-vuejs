7519d7b418bc2a4d302bbc6a84d1be9b26a4d1db
dev-edsonlopes <dev_edson@outlook.com>
2023-09-05 12:35:31 -0300

    CRUD na  pratica

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,93 +1,85 @@
-// console.log('Hello');
-
-/**
- *  Preparação de ambiente
- * 
- *  init projeto
- *      npm init -y
- * 
- *  install package
- *      - typescript e ts-node-dev -D
- *      - express
- *          --@types/express
- *  script execute 
- *      -- "dev": "ts-node-dev --quiet --clear ./src/server.ts"
- *  
- *  typescript config
- *      npx tsc --init
- *          -- rootDir = ./src e outDir = ./dist
- * 
- *  
-**/
-
-
-/* Criando Server
-
-    -- import biblioteca express
-    -- Create variavel para receber o a biblioteca como método.
-    -- variavel.listen('porta do servidor', função de callback())
-    
-*/
-import express  from 'express';
+import express, { response } from "express";
+import { request } from "http";
+import { v4 as uuid } from "uuid";
 
 const app = express();
 
-/**
- * 
- * // URL -> http://localhost:3333/users
- * 
- *  Metodos HTTP
- * 
- * GET, POST, PUT, DELETE -> Esses métodos ele espera o nome da rota, e uma função de callback e essa função ela irá conter dois parametros a requisição e a resposta;
- * 
- * Tipos de parâmentros
- * 
- * Query params -> utilizado para fazer filtros de informção durante a requisição.
- *      Exemplo para não trazer todos os dados de uma requisição pode ser usado  conceito de pagição que seria criado paramentros na rota.
- * 
-            app.get('/users', (request, response) => {
-                // exemplo Query params
-                const {perPage, page} = request.query;
-                console.log(perPage, page);
+/* CRUD */
+app.use(express.json());
 
-                return response.json({ 
-                    message: "Listando usuários",
-                });
-            });
+// Exemplo acadêmico: como não tem banco de dados, os dados não terá persistência só ficará disponivel em momento de execução.
 
- * 
- *  
- * Route params -> Utilizado para indetificar um recurso de uma rota.
- *  Exemplo.:
- * 
-        app.put('/users/:id', (request, response) => {
-            
-            //exemplo Route Params
-            const params = request.params;
-            console.log(params);
+interface User {
+    id: string;
+    name: string;
+    email: string;
+};
 
-            return response.json({ 
-                message: "Atualizando usuários",
-            });
+const users: User [] = [];
 
-        });
- * 
- * Request Body -> Utilizado para enviar dados dentro do corpo da requisição
- * 
-        app.use(express.json())
+app.get('/users', (request, response) => {
+    // Buscar no banco de dados os registro
+            // SEM BANCO
+
+    // Retornar os registro
+    return response.json(users);
+});
+
+app.post('/users', (request, response) => {
+    // Receber os dados para fazer inserção
+    const { name, email } = request.body;
+
+    // Criar um novo registro
+    const user = { id: uuid(), name, email };
+    // Registrar esse usuário na base de dados
+        // SEM BANCO
+        users.push(user);
+
+    // Retornar os dados do usuário criado
+    return response.json(user);
 
 
-        app.post('/users', (request, response) => {
+});
 
-            const body = request.body;
-            console.log(body);
-            return response.json(body);
+app.put('/users/:id', (request, response) => {
+    //Receber os dados
+    const { id } = request.params;
+    const { name, email } = request.body;
 
-        });
- * 
- */
+    // Localizar o registro na base de dados
+    const userIndex = users.findIndex( (user) => user.id === id );
 
+    // tratamento se não houver registro
+    if(userIndex < 0) {
+        return response.status(404).json({ error: "User not found."});
+    }
 
+    // atualizar o registro na base de dados
+    const user = { id, name, email };
+    users[userIndex] = user;
+
+    // retornar os dados do registro atualizado
+    return response.json(user);
+});
+
+app.delete('/users/:id', (request, response) => {
+    //Receber o id do registro
+    const { id } = request.params;
+    // Localizar o registro na base de dados 
+    const userIndex = users.findIndex( user => user.id === id );
+
+    // tratamento se não houver registro
+    if(userIndex < 0) {
+        return response.status(404).json({ error: "User not found."});
+    };
+
+    // Excluir o registro na base de dados
+    users.splice(userIndex, 1);
+
+    // retornar o status da requisição
+    return response.status(204).send();
+
+});
 
 app.listen('3333', () => {
     console.log('Backend Started');
